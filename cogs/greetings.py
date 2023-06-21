a9e1eb01f1253ea3dfa4179f74747654851cfa59
Stephen Lee <stephenlee91@gmail.com>
2023-06-20 20:05:21 -0700

    Added new suggestions: Constant greetings message that requires new users to react to. Created constantvariables.py that contains variables that pertains to each server. Created a media folder full of copyright free images and gifs for use. Created the ability to unreact to welcome message and remove role

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -1,39 +1,53 @@
 import discord
+import constantvariables
+import os
 from discord.ext import commands
 
-# Sends a greeting message that requires a new user to react to the message in order to receive a role to access the server
+# A constant greetings message that is sent to a text channel of your choice and requires a new user to react to receive a role to access the server. Removes role if unreacted to.
 class Greetings(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        channel = self.client.get_channel(1011049173442895902)  # Replace with the actual ID of your welcome text channel
+    @commands.command()
+    async def welcome(self, ctx):
+        channel = self.client.get_channel(constantvariables.welcome_channel_id)
 
         if channel is not None:
+            logo_file = discord.File(os.path.join("media", "copyrightfreelogo.jpg"), filename="copyrightfreelogo.jpg")
+            gif_file = discord.File(os.path.join("media", "copyrightfreegif.gif"), filename="copyrightfreegif.gif")
 
             embed = discord.Embed(
                 title="Hello!",
-                description=f"Welcome to the server {member.mention}!\nPlease read the rules and react to the emoji below if you want access to the server\n\n1. No racism\n2. No bullying\n3. Be kind\n4. Don't ask for roles\n5. Use common sense\n6. Send Parker **LOTS** of kisses\n\nIf you do not follow any of these rules, you will be kicked and potentially banned\n\nEnjoy your stay!",
+                description=f"Welcome to the server!\nPlease read the rules and react to the emoji below if you want access to the server\n\n1. No racism\n2. No bullying\n3. Be kind\n4. Don't ask for roles\n5. Use common sense\n6. Send Parker **LOTS** of kisses\n\nIf you do not follow any of these rules, you will be kicked and potentially banned\n\nEnjoy your stay!",
                 color=discord.Color.blue()
             )
-            self._last_member = member
-            embed.set_author(name="RasenBot", icon_url="https://avatars.githubusercontent.com/u/39735649?s=280&v=4")
-            embed.set_thumbnail(url="http://www.willpap-projects.com/Docus/Educational/Slide_Shows/Gifs/Funny/Cat_Paws_79.gif")
+
+            embed.set_author(name="RasenBot", icon_url="attachment://copyrightfreelogo.jpg")
+            embed.set_thumbnail(url="attachment://copyrightfreegif.gif")
             embed.set_footer(text="If you are curious about RasenBot, please type $help in the bot commands channel for more commands!")
 
-            message = await channel.send(embed=embed)
+            message = await channel.send(embed=embed, files=[logo_file, gif_file])
             await message.add_reaction("👍")
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.channel_id == 1011049173442895902:  # Replace with the actual ID of your welcome text channel
+        if payload.channel_id == constantvariables.welcome_channel_id:
             if "👍" in str(payload.emoji):
                 guild = self.client.get_guild(payload.guild_id)
                 member = guild.get_member(payload.user_id)
                 if member is not None and not member.bot:  # Check if the member exists and is not a bot
-                    role = guild.get_role(1120501110407434301) # Replace with the actual ID of the role you want to assign
+                    role = guild.get_role(constantvariables.verified_member_id)
                     await member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if payload.channel_id == constantvariables.welcome_channel_id:
+            if "👍" in str(payload.emoji):
+                guild = self.client.get_guild(payload.guild_id)
+                member = guild.get_member(payload.user_id)
+                if member is not None and not member.bot:
+                    role = guild.get_role(constantvariables.verified_member_id)
+                    await member.remove_roles(role)
 
 
 def setup(client):
---
+++ constantvariables.py
@@ -0,0 +1,7 @@
+'''
+Change the variables to whatever your server IDs are. Each is labelled accordingly
+'''
+
+welcome_channel_id = 1011049173442895902 # Replace with the actual ID of your welcome text channel
+
+verified_member_id = 1120501110407434301 # Replace with the actual ID of the role you want for the greetings message
