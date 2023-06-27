396264799b290ce74b7bc398fa8894f50b306910
Stephen Lee <stephenlee91@gmail.com>
2023-06-26 17:21:20 -0700

    Added emoji to constantvariables, more efficiency for bot as it is able to remember welcome message after it goes offline

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -3,18 +3,29 @@ import constantvariables
 import os
 from discord.ext import commands
 
-# A constant greetings message that is sent to a text channel of your choice and requires a new user to react to receive a role to access the server. Removes role if unreacted to.
 class Greetings(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.welcome_message_id = self.load_welcome_message_id()
+
+    def load_welcome_message_id(self):
+        try:
+            with open("txtfiles/welcome_message_id.txt", "r") as file:
+                return int(file.read())
+        except FileNotFoundError:
+            return None
+
+    def save_welcome_message_id(self, message_id):
+        with open("txtfiles/welcome_message_id.txt", "w") as file:
+            file.write(str(message_id))
 
     @commands.command()
     async def welcome(self, ctx):
-        channel = self.client.get_channel(constantvariables.welcome_channel_id)
+        channel = self.client.get_channel(constantvariables.WELCOME_CHANNEL_ID)
 
         if channel is not None:
-            logo_file = discord.File(os.path.join("media", "copyrightfreelogo.jpg"), filename="copyrightfreelogo.jpg")
-            gif_file = discord.File(os.path.join("media", "copyrightfreegif.gif"), filename="copyrightfreegif.gif")
+            logo_file = discord.File(os.path.join("media", "rasenbotlogo.jpg"), filename="rasenbotlogo.jpg")
+            gif_file = discord.File(os.path.join("media", "discordlogogif.gif"), filename="discordlogogif.gif")
 
             embed = discord.Embed(
                 title="Hello!",
@@ -22,33 +33,36 @@ class Greetings(commands.Cog):
                 color=discord.Color.blue()
             )
 
-            embed.set_author(name="RasenBot", icon_url="attachment://copyrightfreelogo.jpg")
-            embed.set_thumbnail(url="attachment://copyrightfreegif.gif")
+            embed.set_author(name="RasenBot", icon_url="attachment://rasenbotlogo.jpg")
+            embed.set_thumbnail(url="attachment://discordlogogif.gif")
             embed.set_footer(text="If you are curious about RasenBot, please type $help in the bot commands channel for more commands!")
 
             message = await channel.send(embed=embed, files=[logo_file, gif_file])
-            await message.add_reaction("👍")
+            await message.add_reaction(constantvariables.WELCOME_REACT_EMOJI)
             self.welcome_message_id = message.id
+            self.save_welcome_message_id(message.id)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.channel_id == constantvariables.welcome_channel_id and payload.message_id == self.welcome_message_id:
-            if "👍" in str(payload.emoji):
-                guild = self.client.get_guild(payload.guild_id)
-                member = guild.get_member(payload.user_id)
-                if member is not None and not member.bot:  # Check if the member exists and is not a bot
-                    role = guild.get_role(constantvariables.verified_member_id)
-                    await member.add_roles(role)
+        if payload.message_id == self.welcome_message_id:
+            if payload.channel_id == constantvariables.WELCOME_CHANNEL_ID:
+                if constantvariables.WELCOME_REACT_EMOJI in str(payload.emoji):
+                    guild = self.client.get_guild(payload.guild_id)
+                    member = guild.get_member(payload.user_id)
+                    if member is not None and not member.bot:
+                        role = guild.get_role(constantvariables.VERIFIED_MEMBER_ID)
+                        await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        if payload.channel_id == constantvariables.welcome_channel_id and payload.message_id == self.welcome_message_id:
-            if "👍" in str(payload.emoji):
-                guild = self.client.get_guild(payload.guild_id)
-                member = guild.get_member(payload.user_id)
-                if member is not None and not member.bot:
-                    role = guild.get_role(constantvariables.verified_member_id)
-                    await member.remove_roles(role)
+        if payload.message_id == self.welcome_message_id:
+            if payload.channel_id == constantvariables.WELCOME_CHANNEL_ID:
+                if constantvariables.WELCOME_REACT_EMOJI in str(payload.emoji):
+                    guild = self.client.get_guild(payload.guild_id)
+                    member = guild.get_member(payload.user_id)
+                    if member is not None and not member.bot:
+                        role = guild.get_role(constantvariables.VERIFIED_MEMBER_ID)
+                        await member.remove_roles(role)
 
 
 def setup(client):
--- constantvariables.py
+++ constantvariables.py
@@ -2,6 +2,8 @@
 Change the variables to whatever your server IDs are. Each is labelled accordingly
 '''
 
-welcome_channel_id = 1011049173442895902 # Replace with the actual ID of your welcome text channel
+WELCOME_CHANNEL_ID = 1011049173442895902 # Replace with the actual ID of your welcome text channel
 
-verified_member_id = 1120501110407434301 # Replace with the actual ID of the role you want for the greetings message
+VERIFIED_MEMBER_ID = 1120501110407434301 # Replace with the actual ID of the role you want for the greetings message
+
+WELCOME_REACT_EMOJI = "👍" # Replace with the emoji you want to use for your welcome message react emoji
