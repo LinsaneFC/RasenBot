0d9d200a029176959ac6a7cd21a6d8c99c9626cb
Benjamin Lin <benjilin25@gmail.com>
2023-08-23 01:49:36 -0700

    displayinfo now works when added into multiple servers, members_online is a dictionary that holds user ids and server ids as keys and values respectively

--- cogs/displayinfo.py
+++ cogs/displayinfo.py
@@ -34,9 +34,8 @@ class DisplayInfo(commands.Cog):
         self.bot = bot
         self.current_collection = db["UserTracking"] # TODO: Move this secrets file
         self.timers = self.current_collection.find_one() # Gets document from collection.
-        self.current_guild_name = ""
         self.current_date = str(date.today())
-        self.members_online = set()
+        self.members_online = {}
         self.timed_task = None
     
     '''
@@ -48,15 +47,16 @@ class DisplayInfo(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
+        # print(self.bot.guilds)
         for guild in self.bot.guilds:
             for member in guild.members:
                 if member.status == discord.Status.online and member.activity:
-                    self.members_online.add(member)
+                    self.members_online[member.id] = guild.id
                     await self.reset_start(member)
                     await self._update_now(member)
                    
 
-        print(self.members_online)
+        # print(self.members_online)
 
         if len(self.members_online):
             self.timed_task = asyncio.create_task(self.timed_update()) #start timed_update once bot goes online if a member is online
@@ -91,48 +91,49 @@ class DisplayInfo(commands.Cog):
     # The 'mem_before' parameter represents the member's previous state, and 'mem_after' represents the current state.
     @commands.Cog.listener()
     async def on_member_update(self, mem_before:discord.Member, mem_after:discord.Member):
-        self.current_guild_name = mem_before.guild.name
-        
         if mem_before.activity and mem_before.activity == mem_after.activity:
             return
 
         # Track activity updates for members
         if mem_after.status == discord.Status.online and (not mem_before.activity and mem_after.activity): # activity starts/ongoing
-            if mem_after not in self.members_online:
-                self.members_online.add(mem_after)
+            if mem_after.id not in self.members_online:
+                self.members_online[mem_after.id] = mem_after.guild.id
                 await self.reset_start(mem_after)
 
                 if self.timed_task == None: #if task is finished create new task
                     self.timed_task = asyncio.create_task(self.timed_update())
                 print("added:", mem_after.name)
-
-            update_success = await self._update_now(mem_after)
-            print("start:", self.timers)
+            
+            if self.members_online[mem_after.id] == mem_after.guild.id:
+                update_success = await self._update_now(mem_after)
+                print("start:", self.timers)
         elif (mem_before.activity and not mem_after.activity) or (mem_before.status != mem_after.status and mem_after.status == discord.Status.offline): # activity stopped or member goes offline
-            if mem_before in self.members_online:
-                self.members_online.remove(mem_before)
+            if mem_before.id in self.members_online:
+                self.members_online.pop(mem_before.id)
                 print("removed:", mem_before.name)
                 if len(self.members_online) <= 0:
                     self.timed_task.cancel()    
                     self.timed_task = None
-
-            before_string_id = str(mem_before.id)
-            if before_string_id in self.timers:
-                if self.current_date in self.timers[before_string_id]:
-                    if mem_before.activity and mem_before.activity.name in self.timers[before_string_id][self.current_date]:
-                        currentStartTime = self.timers[before_string_id][self.current_date][mem_before.activity.name]["startTime"]
-                        self.timers[before_string_id][self.current_date][mem_before.activity.name]["startTime"] = None
-                        if currentStartTime:
-                            playTime = datetime.now() - currentStartTime # Calculate the time difference (playTime) between the start and end time of the activity
-                            self.timers[before_string_id][self.current_date][mem_before.activity.name]["totalTime"] = self.timers[before_string_id][self.current_date][mem_before.activity.name].get("totalTime", 0) + playTime.total_seconds() # Add the play time to the member's total play time for that activity for that day
-                        print("end:", self.timers) # printing the dictionary to see the updates after an activity is ended
-                        self.current_collection.replace_one({}, self.timers) # Update the MongoDB collection with the updated activity data(self.timers)
+                    
+                before_string_id = str(mem_before.id)
+                if before_string_id in self.timers:
+                    if self.current_date in self.timers[before_string_id]:
+                        if mem_before.activity and mem_before.activity.name in self.timers[before_string_id][self.current_date]:
+                            currentStartTime = self.timers[before_string_id][self.current_date][mem_before.activity.name]["startTime"]
+                            self.timers[before_string_id][self.current_date][mem_before.activity.name]["startTime"] = None
+                            if currentStartTime:
+                                playTime = datetime.now() - currentStartTime # Calculate the time difference (playTime) between the start and end time of the activity
+                                self.timers[before_string_id][self.current_date][mem_before.activity.name]["totalTime"] = self.timers[before_string_id][self.current_date][mem_before.activity.name].get("totalTime", 0) + playTime.total_seconds() # Add the play time to the member's total play time for that activity for that day
+                            print("end:", self.timers) # printing the dictionary to see the updates after an activity is ended
+                            self.current_collection.replace_one({}, self.timers) # Update the MongoDB collection with the updated activity data(self.timers)
 
     #This function periodically updates the activities of members that are online and in an activity.
     async def timed_update(self):
         while len(self.members_online) > 0:
             await asyncio.sleep(300)
-            for member in self.members_online:
+            for memberID, guildID in self.members_online.items():
+                guild = self.bot.get_guild(guildID)
+                member = guild.get_member(memberID)
                 update_success = await self._update_now(member)
             
             print("timed update:", self.timers)
