5a82b17993f44960b4ab562ec998950250b31eb2
Benjamin Lin <benjilin25@gmail.com>
2023-09-07 20:32:45 -0700

    added a timer at midnight, updates active members at 11:59, updates self.current_date and deletes previous days startTimes, made other small changes according to comments

--- cogs/displayinfo.py
+++ cogs/displayinfo.py
@@ -2,8 +2,7 @@ import discord
 import mongoDB
 from discord.ext import commands
 from discord.ext import tasks
-from datetime import datetime
-from datetime import date
+from datetime import datetime, date, time
 import datetime as dt
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -47,19 +46,19 @@ class DisplayInfo(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
-        # print(self.bot.guilds)
+        await self._next_day_check()
         for guild in self.bot.guilds:
             for member in guild.members:
                 if member.status == discord.Status.online and member.activity:
                     self.members_online[member.id] = guild.id
-                    await self.reset_start(member)
+                    await self._reset_start(member)
                     await self._update_now(member)
                    
 
         # print(self.members_online)
 
         if len(self.members_online):
-            self.timed_task = asyncio.create_task(self.timed_update()) #start timed_update once bot goes online if a member is online
+            self.timed_task = asyncio.create_task(self._timed_update()) #start timed_update once bot goes online if a member is online
 
     async def _update_now(self, member: discord.Member):
         updated = False
@@ -75,7 +74,7 @@ class DisplayInfo(commands.Cog):
 
         if not updated:
             if string_id not in self.timers:
-                    self.timers[string_id] = {} # If the member is not already tracked, add them
+                self.timers[string_id] = {} # If the member is not already tracked, add them
             if self.current_date not in self.timers[string_id]:
                 self.timers[string_id][self.current_date] = {} # If the current date is not present in the member's dictionary, add it
             if member.activity.name not in self.timers[string_id][self.current_date]: # If the member's current activity/game/status is not present, add it with initial values
@@ -87,6 +86,62 @@ class DisplayInfo(commands.Cog):
             
 
         return updated
+    
+    #This function periodically updates the activities of members that are online and in an activity.
+    async def _timed_update(self):
+        while len(self.members_online) > 0:
+            await asyncio.sleep(300)
+            for memberID, guildID in self.members_online.items():
+                guild = self.bot.get_guild(guildID)
+                member = guild.get_member(memberID)
+                update_success = await self._update_now(member)
+            
+            print("timed update:", self.timers)
+            self.current_collection.replace_one({}, self.timers)
+
+    #This function resets all the startTimes of a member
+    async def _reset_start(self, member):
+        string_id = str(member.id)
+        if string_id in self.timers and self.current_date in self.timers[string_id]:
+            for activity, info in self.timers[string_id][self.current_date].items():
+                info["startTime"] = None
+
+
+    async def _next_day_check(self):
+        while True:
+            await asyncio.sleep(self._seconds_until())  # Will sleep until clock says 11:59:00
+            for memberID, guildID in self.members_online.items():
+                guild = self.bot.get_guild(guildID)
+                member = guild.get_member(memberID)
+                update_success = await self._update_now(member) #update all with current time
+
+            print(self.timers)
+            ids = [id for id, dates in self.timers.items()][1:]
+
+            for id in ids: #for loop to delete previous day startTime
+                print(id)
+                string_id = str(id)
+                if string_id in self.timers and self.current_date in self.timers[string_id]:
+                    for activity, info in self.timers[string_id][self.current_date].items():
+                        del info["startTime"]
+
+            await asyncio.sleep(60) #Sleep till the next day
+            if str(date.today() - dt.timedelta(days=1)) == self.current_date: #check if it is the next day  
+                self.current_date = str(date.today())
+                for memberID, guildID in self.members_online.items():
+                    guild = self.bot.get_guild(guildID)
+                    member = guild.get_member(memberID)
+                    update_success = await self._update_now(member)
+                
+                self.current_collection.replace_one({}, self.timers)
+
+    def _seconds_until(self): #finds the seconds needed to 11:59 of that day
+        now = datetime.now()
+        curr = datetime.strptime(self.current_date, "%Y-%m-%d")
+        midnight = datetime(curr.year, curr.month, curr.day, 23,59)
+
+        return (midnight - now).total_seconds()
+    
         
     # The 'mem_before' parameter represents the member's previous state, and 'mem_after' represents the current state.
     @commands.Cog.listener()
@@ -98,13 +153,13 @@ class DisplayInfo(commands.Cog):
         if mem_after.status == discord.Status.online and (not mem_before.activity and mem_after.activity): # activity starts/ongoing
             if mem_after.id not in self.members_online:
                 self.members_online[mem_after.id] = mem_after.guild.id
-                await self.reset_start(mem_after)
+                await self._reset_start(mem_after)
 
                 if self.timed_task == None: #if task is finished create new task
-                    self.timed_task = asyncio.create_task(self.timed_update())
+                    self.timed_task = asyncio.create_task(self._timed_update())
                 print("added:", mem_after.name)
             
-            if self.members_online[mem_after.id] == mem_after.guild.id:
+            if  mem_after.guild.id == self.members_online[mem_after.id]:
                 update_success = await self._update_now(mem_after)
                 print("start:", self.timers)
         elif (mem_before.activity and not mem_after.activity) or (mem_before.status != mem_after.status and mem_after.status == discord.Status.offline): # activity stopped or member goes offline
@@ -126,26 +181,6 @@ class DisplayInfo(commands.Cog):
                                 self.timers[before_string_id][self.current_date][mem_before.activity.name]["totalTime"] = self.timers[before_string_id][self.current_date][mem_before.activity.name].get("totalTime", 0) + playTime.total_seconds() # Add the play time to the member's total play time for that activity for that day
                             print("end:", self.timers) # printing the dictionary to see the updates after an activity is ended
                             self.current_collection.replace_one({}, self.timers) # Update the MongoDB collection with the updated activity data(self.timers)
-
-    #This function periodically updates the activities of members that are online and in an activity.
-    async def timed_update(self):
-        while len(self.members_online) > 0:
-            await asyncio.sleep(300)
-            for memberID, guildID in self.members_online.items():
-                guild = self.bot.get_guild(guildID)
-                member = guild.get_member(memberID)
-                update_success = await self._update_now(member)
-            
-            print("timed update:", self.timers)
-            self.current_collection.replace_one({}, self.timers)
-
-    #This function resets all the startTimes of a member
-    async def reset_start(self, member):
-        string_id = str(member.id)
-        if string_id in self.timers and self.current_date in self.timers[string_id]:
-            for activity, info in self.timers[string_id][self.current_date].items():
-                info["startTime"] = None
-            
 
     # This is a Discord bot command that retrieves and displays activity time information for a given member.
     @commands.command()
